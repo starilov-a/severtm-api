5eb2117a5ddcd4c093af36c3e5c35ad88d13e8e3
starilov <anton_starilov@mail.ru>
2025-05-15 15:24:03 +0300

    add debug tools

--- src/Modules/UserCabinet/Controllers/APIv1/Controller.php
+++ src/Modules/UserCabinet/Controllers/APIv1/Controller.php
@@ -3,16 +3,17 @@
 
 namespace App\Modules\UserCabinet\Controllers\APIv1;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use function Symfony\Component\Console\CI\debug;
 
-class Controller
+class Controller extends AbstractController
 {
     #[Route('/get-short-user-info', name: 'getShortUserInfo', methods: ['GET'])]
     public function getShortUserInfo(): Response
     {
         return new Response('Краткая информация о пользователе');
-
-        dd(123);
     }
 }
