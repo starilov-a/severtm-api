ee4d8eeea7342ae5dc64a04e8b8d11a351668226
starilov <anton_starilov@mail.ru>
2025-10-20 22:21:20 +0300

    update auth

--- src/Modules/UserCabinet/EventListener/AuthListener.php
+++ src/Modules/UserCabinet/EventListener/AuthListener.php
@@ -3,7 +3,7 @@
 namespace App\Modules\UserCabinet\EventListener;
 
 use App\Modules\Common\Infrastructure\Exception\AuthException;
-use App\Modules\Common\Infrastructure\Service\Auth\Service\UserSessionService;
+use App\Modules\Common\Infrastructure\Service\Auth\Service\Auth;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -12,20 +12,25 @@ use Symfony\Component\HttpKernel\KernelEvents;
 final class AuthListener
 {
     protected \Throwable $e;
-    public function onKernelController(ControllerEvent $event): void
+    public function onKernelController(ControllerEvent $event, Auth $auth): void
     {
         session_start();
+
         $controller = $event->getController();
-
-        if (is_array($controller)) {
-            [$controllerObject] = $controller;
-
-            if (method_exists($controllerObject, 'authenticate')) {
-                $result = $controllerObject->authenticate();
-                if ($result && !UserSessionService::checkAuth()) {
-                    throw new AuthException("User is not found");
-                }
-            }
+        if (!is_array($controller)) {
+           return;
         }
+
+        [$controllerObject] = $controller;
+
+        if (!method_exists($controllerObject, 'authenticate')) {
+            return;
+        }
+
+        if (!$controllerObject->authenticate() || !$auth->checkAuth()) {
+            return;
+        }
+
+        throw new AuthException("Необходима ");
     }
 }
