e42ffb8f9ab16f0e475e4f2ad076b05ae0b8eebb
starilov <anton_starilov@mail.ru>
2025-11-07 10:40:53 +0300

    итеррация 2

---
+++ src/Modules/UserCabinet/Controllers/APIv1/PaymentsController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\UserCabinet\Controllers\APIv1;
+
+use App\Modules\UserCabinet\Controllers\APIv1\Controller;
+
+class PaymentsController extends Controller
+{
+
+}
---
+++ src/Modules/UserCabinet/Controllers/APIv1/ServController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Modules\UserCabinet\Controllers\APIv1;
+
+class ServServiceController extends Controller
+{
+
+}
